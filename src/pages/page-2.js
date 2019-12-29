68c7b8750f95e230af078181a2f30eee73343652
pholmq <pholmq@gmail.com>
2019-12-30 00:10:57 +0100

    update

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,7 +10,18 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+
   </Layout>
 )
-
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "headers/headshot.jpg" }) {
+      childImageSharp {
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 export default SecondPage
